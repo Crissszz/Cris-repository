d536d68f0558c704dce40a01394cfb3d9cc97ea1
Crissszz <crisqmerino@gmail.com>
2025-06-24 14:58:40 -0400

    Three Changes

---
+++ Diccionarioa.py
@@ -0,0 +1,15 @@
+diccionario = {
+    1:["Cristobal"],
+    2:["Misael"],
+    3:["Pablo"],
+    4:["Benja"],
+    5:["Thomi"],
+    6:["Gabo"],
+    7:["Colo"],
+    8:["Flo"],
+}
+for i in diccionario:
+    print(diccionario[i])
+
+
+
