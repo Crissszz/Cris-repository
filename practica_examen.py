429e76257570402a606e7092ef6b07f958d6a410
Crissszz Valenzuela <crisqmerino@gmail.com>
2025-07-01 12:55:27 -0400

    Add files via upload

---
+++ practica_examen.py
@@ -0,0 +1,23 @@
+# album {album_id:[titulo,genero,artista,años_lanzamiento, formato]...}
+albums = {  
+'albm01':["Un Verano Sin Ti", "Reggaeton", "Bad_Bunny", 2022, "Disco"],
+'albm02':["Thriller", "Pop", "Michael Jackson", 1982, "CD"],
+'albm03':["Soy como quiero ser", "Latin Pop RYB" , "Luis Miguel", 1987, "Vinilo"],
+'albm04':["Romance", "Bolero" , "Luis Miguel", 1992, "Vinilo"],
+'albm05':["DTMF", "Reggaeton Salsa" , "Bad Bunny", 2025, "Disco"],
+'albm06':["Apocalipsis", "Reggaeton" , "Cris Mj", 2025, "Disco"],
+'albm07':["Chromakopia", "Jazz RyB Contemponareo" , "Tyler The Creator", "2025", "Disco"],
+          }
+
+# Inventario {album_id:[precio , stock,]...} 
+inventario = {
+'albm01': [6],
+'albm02': [1],
+'albm03': [3],
+'albm04': [7],
+'albm05': [0],
+'albm06': [2],
+'albm07': [1],
+}
+def Menu_Principal():
+    print("---Menu Principal---")
